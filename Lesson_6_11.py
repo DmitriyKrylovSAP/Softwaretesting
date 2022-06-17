c7a5afdb732374a1c187a07d6a72998d209c3061
dmitrijkrylov <dkrylovsap@gmail.com>
2022-06-17 14:39:55 +0300

    задание 11

---
+++ Lesson_6_11.py
@@ -0,0 +1,45 @@
+import time
+import faker
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
+def test_all_chapter(driver):
+    driver.get("http://localhost/litecart/en/create_account")
+    # регистрация
+    fake = faker.Faker('en_US')
+    fake_phone = fake.bothify(text='+168########')
+    driver.find_element_by_name("phone").send_keys(fake_phone)
+    driver.find_element_by_name("firstname").send_keys(fake.first_name())
+    driver.find_element_by_name("lastname").send_keys(fake.last_name())
+    driver.find_element_by_name("address1").send_keys(fake.city())
+    driver.find_element_by_name("postcode").send_keys(fake.postcode())
+    driver.find_element_by_name("city").send_keys(fake.city())
+    passw = fake.pyint(8)
+    mail = fake.email()
+    driver.find_element_by_name("email").send_keys(mail)
+    driver.find_element_by_name("password").send_keys(passw)
+    driver.find_element_by_name("confirmed_password").send_keys(passw)
+    driver.find_element_by_xpath("//*[text()='United States']").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("//*[text()='Alaska']").click()
+    driver.find_element_by_name("create_account").click()
+    # выход
+    driver.find_element_by_xpath("//*[text()='Logout']").click()
+    driver.find_element_by_name("email").send_keys(mail)
+    driver.find_element_by_name("password").send_keys(passw)
+    # вход
+    driver.find_element_by_xpath("//*[@name='login']").click()
+    # выход
+    driver.find_element_by_xpath("//*[text()='Logout']").click()
+
+
